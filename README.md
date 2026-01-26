fc71d58ea2d7150434298966aa1d880b8c33cc43
Bisthum <imbisthum@gmail.com>
2026-01-26 04:38:07 +0000

    bah

--- README.md
+++
@@ -1,6 +0,0 @@
-# Creations By Lani
-*"Handmade goods made to delight."*
-
-
-### My Current Progress:
-Currrently, what I have done is a rough draft of the **landing page.** The email subscription service is still being worked on, because it is much more complex than I thought it would be.
---
+++ unique-collections-by-lani/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Unique Collecitons By Lani",
+  description: "Handmade goods made to delight.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Unique Collections By Lani</title>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
